b2e80c63983e5fa09b9434a9eb3ebd48c6217d87
Manoj Shrestha <manojshrestha@Manojs-MacBook-Air.local>
2024-09-20 15:16:43 +0545

    changes added to voter side barl

--- frontend/src/app/components/VoterSidebar.tsx
+++ frontend/src/app/components/VoterSidebar.tsx
@@ -1,6 +1,10 @@
 "use client"
 import Link from "next/link";
 import {useRouter} from 'next/navigation'
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineHowToVote } from "react-icons/md";
+import { MdQueryStats } from "react-icons/md";
+import { IoIosLogOut } from "react-icons/io";
 
 // Sidebar component
 const VoterSidebar = () => {
@@ -17,23 +21,27 @@ const VoterSidebar = () => {
       <nav className="mt-8">
         <ul className="space-y-4">
           <li>
-            <Link href="/voter/profile" className="block py-2 px-4 hover:bg-blue-700">
-              Profile
+            <Link href="/voter/profile" className="flex items-center py-2 px-4 hover:bg-blue-700">
+            <CgProfile className="mr-3 text-2xl"/>
+            <span>Profile</span>
             </Link>
           </li>
           <li>
-            <Link href="/voter/settings" className="block py-2 px-4 hover:bg-blue-700">
-              Voting Area
+            <Link href="/voter/settings" className="flex items-center py-2 px-4 hover:bg-blue-700">
+            <MdOutlineHowToVote className="mr-3 text-2xl"/>
+              <span>Voting Area</span>
             </Link>
           </li>
           <li>
-            <Link href="/voter/vote" className="block py-2 px-4 hover:bg-blue-700">
-              Result
+            <Link href="/voter/vote" className="flex items-center py-2 px-4 hover:bg-blue-700">
+            <MdQueryStats className="mr-3 text-2xl"/>
+              <span>Result</span>
             </Link>
           </li>
           <li>
-            <button onClick={handleLogout} className="block w-full py-2 px-4 text-left hover:bg-blue-700">
-              Logout
+            <button onClick={handleLogout} className="flex items-center py-2 px-4 text-left hover:bg-blue-700">
+            <IoIosLogOut className="mr-3 text-2xl"/>
+              <span>Logout</span>
             </button>
           </li>
         </ul>
