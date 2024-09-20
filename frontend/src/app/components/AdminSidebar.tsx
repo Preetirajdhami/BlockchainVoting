4b33e0f0dc26a3af0d6674104786b071e5cf4b62
Manoj Shrestha <manojshrestha@Manojs-MacBook-Air.local>
2024-09-20 15:40:05 +0545

    added changes to voter sidebar and admin sidebar

--- frontend/src/app/components/AdminSidebar.tsx
+++ frontend/src/app/components/AdminSidebar.tsx
@@ -1,6 +1,10 @@
 "use client"
 import Link from "next/link";
 import { useRouter } from 'next/navigation';
+import { FaRegAddressCard } from "react-icons/fa6";
+import { IoAddCircleOutline } from "react-icons/io5";
+import { IoStatsChartSharp } from "react-icons/io5";
+import { IoIosLogOut } from "react-icons/io";
 
 // Sidebar component
 const AdminSidebar = () => {
@@ -18,23 +22,27 @@ const AdminSidebar = () => {
             <nav className="mt-8">
                 <ul className="space-y-4">
                     <li>
-                        <Link href="/admin/candidate-details" className="block py-2 px-4 hover:bg-blue-700">
-                            Candidate Details
+                        <Link href="/admin/candidate-details" className="flex items-center py-2 px-4 hover:bg-blue-700">
+                        <FaRegAddressCard className="mr-3 text-2xl"/>
+                           <span>Candidate Details</span> 
                         </Link>
                     </li>
                     <li>
-                        <Link href="/admin/add-candidate" className="block py-2 px-4 hover:bg-blue-700">
-                            Add Candidate
+                        <Link href="/admin/add-candidate" className="flex items-center py-2 px-4 hover:bg-blue-700">
+                        <IoAddCircleOutline className="mr-3 text-2xl"/>
+                           <span>Add Candidate</span> 
                         </Link>
                     </li>
                     <li>
-                        <Link href="/admin/voting-status" className="block py-2 px-4 hover:bg-blue-700">
-                            Voting Status
+                        <Link href="/admin/voting-status" className="flex items-center py-2 px-4 hover:bg-blue-700">
+                        <IoStatsChartSharp className="mr-3 text-xl"/>
+                            <span>Voting Status</span>
                         </Link>
                     </li>
                     <li>
-                        <button onClick={handleLogout} className="block w-full py-2 px-4 text-left hover:bg-blue-700">
-                            Logout
+                        <button onClick={handleLogout} className="flex items-center py-2 px-4 text-left hover:bg-blue-700">
+                        <IoIosLogOut className="mr-3 text-2xl"/>
+                        <span>Logout</span>
                         </button>
                     </li>
                 </ul>
