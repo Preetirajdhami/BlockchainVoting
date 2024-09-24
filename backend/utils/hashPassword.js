cad387bd7ebf793e68764b7f312a4a23e7168c7b
Preetirajdhami <preetirajdhami@gmail.com>
2024-09-25 00:24:00 +0545

    Updated .gitignore

--- backend/utils/hashPassword.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const password = 'quickvote123'; 
-const saltRounds = 10;
-
-bcrypt.hash(password, saltRounds, (err, hash) => {
-  if (err) throw err;
-  console.log(hash); 
-});
