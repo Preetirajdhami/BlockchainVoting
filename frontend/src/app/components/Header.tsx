06291d85e071dc2002a1970190c2cb071e540467
ManojScripts-dot <stha8841@gmail.com>
2024-09-26 00:35:33 +0545

    Update Header.tsx

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Link from 'next/link'; 
 
 const Header = () => {
   return (
     <div className='bg-navBlue text-bold text-2xl text-white lg:px-32 sm:px-6 md:px-8 mx-auto flex items-center justify-between py-4 px-4 relative'>
-      <div>Quick Vote</div>
+      <Link href="/" className="hover:button text-2xl font-bold">
+        Quick Vote
+      </Link>
     </div>
   )
 }
 
-export default Header
+export default Header;
