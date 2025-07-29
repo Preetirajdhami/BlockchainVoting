c342527bb5804d47e0559695bf08724bb80117d8
preeti <preetirajdhami@gmail.com>
2025-07-29 23:59:31 +0545

    truffle

--- Ethereum/truffle-config.js
+++ Ethereum/truffle-config.js
@@ -1,31 +1,32 @@
 require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-const { PRIVATE_KEY, INFURA_PROJECT_ID } = process.env;
+// Make sure to include ALCHEMY_API_URL
+const { PRIVATE_KEY, ALCHEMY_API_URL } = process.env;
 
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",     
-      port: 7545,            
-      network_id: "*",       
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
     },
 
     sepolia: {
       provider: () => new HDWalletProvider(
         PRIVATE_KEY,
-        `https://sepolia.infura.io/v3/${INFURA_PROJECT_ID}`
+        ALCHEMY_API_URL
       ),
-      network_id: 11155111,    // Sepolia's network ID
+      network_id: 11155111, // Sepolia testnet
       confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true
-    },
+    }
   },
 
   compilers: {
     solc: {
-      version: "0.8.0",
+      version: "0.8.0"
     }
   }
 };
